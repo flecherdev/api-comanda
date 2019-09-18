6ffe4664e012e28064a719e096cbc434da45fb73
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2019-09-17 23:11:10 -0300

    se generan los primeros endpoint de empleado

--- src/app/modelORM/empleadoControler.php
+++ src/app/modelORM/empleadoControler.php
@@ -8,35 +8,56 @@ include_once __DIR__ . '../../modelAPI/IApiControler.php';
 
 class empleadoControler implements IApiControler {
     
-  public function TraerTodos($request, $response, $args) {
-    //return cd::all()->toJson();
-    $todosLosCds=empleado::all();
-    $newResponse = $response->withJson($todosLosCds, 200);  
+  public function TraerTodos($request, $response, $args) { //OK
+    $todosLosEmpleados=empleado::all();
+    $newResponse = $response->withJson($todosLosEmpleados, 200);  
     return $newResponse;
   }
 
-  public function TraerUno($request, $response, $args) {
-    //complete el codigo
+  public function TraerUno($request, $response, $args) { // OK
     $traerUno = empleado::find($args['id']);
     $newResponse = $response->withJson($traerUno, 200);  
     return $newResponse;
   }
    
-  public function CargarUno($request, $response, $args) {
-    //complete el codigo
-    $newResponse = $response->withJson("sin completar", 200);  
-    return $response;
+  public function CargarUno($request, $response, $args) { 
+    $dato = json_decode(json_encode($request->getParsedBody()));
+    
+    $miEmpleado = new empleado;
+    $miEmpleado->nombre_empleado = $dato->nombre_empleado;
+    $miEmpleado->id_tipo = $dato->id_tipo ;
+    $miEmpleado->clave_empleado = $dato->clave_empleado;
+  //  $miEmpleado->estado_empleado = $dato->estado_empleado;
+    $miEmpleado->created_at = $dato->created_at;
+    $miEmpleado->updated_at = $dato->updated_at;
+
+    $miEmpleado->save();
+
+    $newResponse = $response->withJson($miEmpleado , 200);  
+    return $newResponse;
   }
 
-  public function BorrarUno($request, $response, $args) {
-    //complete el codigo
-    $newResponse = $response->withJson("sin completar", 200);  
+  public function BorrarUno($request, $response, $args) { //ok
+    $empleado = empleado::destroy($args['id']);
+    $newResponse = $response->withJson($empleado , 200);  
     return $newResponse;
   }
      
   public function ModificarUno($request, $response, $args) {
-    //complete el codigo
-    $newResponse = $response->withJson("sin completar", 200);  
+    $dato = json_decode(json_encode($request->getParsedBody()));
+
+    $miEmpleado = empleado::find($args['id']);
+
+    $miEmpleado->nombre_empleado = $dato->nombre_empleado;
+    $miEmpleado->id_tipo = $dato->id_tipo ;
+    $miEmpleado->clave_empleado = $dato->clave_empleado;
+    $miEmpleado->created_at = $dato->created_at;
+    $miEmpleado->updated_at = $dato->updated_at;
+
+    $miEmpleado->save();
+    
+    $newResponse = $response->withJson($miEmpleado, 200);  
     return 	$newResponse;
-  }  
+  }
+
 }
