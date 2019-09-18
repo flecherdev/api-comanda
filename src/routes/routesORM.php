ee1d978b41702a91be4223967b80d61becb4d1b0
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2019-09-17 23:07:00 -0300

    se agrega las rutas para empleado

--- src/routes/routesORM.php
+++ src/routes/routesORM.php
@@ -4,11 +4,14 @@ use Slim\App;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use App\Models\ORM\cd;
-// use App\Models\ORM\cdApi;
+use App\Models\ORM\empleado;
+
 use App\Models\ORM\cdControler;
+use App\Models\ORM\empleadoControler;
 
 include_once __DIR__ . '/../../src/app/modelORM/cd.php';
 include_once __DIR__ . '/../../src/app/modelORM/cdControler.php';
+include_once __DIR__ . '/../../src/app/modelORM/empleadoControler.php';
 
 return function (App $app) {
     $container = $app->getContainer();
@@ -20,6 +23,15 @@ return function (App $app) {
       $this->post('/cd/add', cdControler::class . ':CargarUno'); //0k
       $this->delete('/cd/delete/[{id}]', cdControler::class . ':BorrarUno');  
       $this->put('/cd/[{id}]', cdControler::class . ':ModificarUno');   
+    });
+
+    // Empleado
+    $app->group('/empleado-orm', function () { 
+      $this->get('/', empleadoControler::class . ':TraerTodos'); //ok
+      $this->get('/empleado/[{id}]', empleadoControler::class . ':TraerUno'); 
+      $this->post('/empleado/add', empleadoControler::class . ':CargarUno'); 
+      $this->delete('/empleado/delete/[{id}]', empleadoControler::class . ':BorrarUno');  
+      $this->put('/empleado/[{id}]', empleadoControler::class . ':ModificarUno');   
     });
 
 };
