01adcd2686ece83a23b544381fe6db18f2c46ba1
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2019-09-17 23:08:24 -0300

    se modifica empleado

--- src/app/modelORM/empleado.php
+++ src/app/modelORM/empleado.php
@@ -3,4 +3,19 @@ namespace App\Models\ORM;
 
 class empleado extends \Illuminate\Database\Eloquent\Model {  
     public $timestamps = false;
+    // Nombre de la tabla
+    protected $table = 'empleados';
+    // Primary Key
+    protected $primaryKey = 'id_empleado';
+    // Filas de la tabla
+    // protected $fillable = [
+    //     "id_empleado",
+    //     "nombre_empleado",
+    //     "id_tipo",
+    //     "clave_empleado",
+    //     "estado_empleado",
+    //     "created_at",
+    //     "updated_at"
+    // ]; 
+
 }
