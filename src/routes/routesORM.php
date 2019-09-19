54057b41f8da5e614af2e18d7ad3743bb05ef9d4
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2019-09-18 23:39:28 -0300

    se borran archivos del modelo y se borra grupo de rutas

--- src/routes/routesORM.php
+++ src/routes/routesORM.php
@@ -1,35 +1,17 @@
 <?php
 
 use Slim\App;
-// use Slim\Http\Request;
-// use Slim\Http\Response;
-// use App\Models\ORM\cd;
-// use App\Models\ORM\empleado;
-// use App\Models\ORM\Estado;
 
-use App\Models\ORM\cdControler;
 use App\Models\ORM\empleadoControler;
 use App\Models\ORM\EstadoControler;
-use App\Models\ORM\Tipo;
 use App\Models\ORM\TipoControler;
 
-include_once __DIR__ . '/../../src/app/modelORM/cd.php';
-include_once __DIR__ . '/../../src/app/modelORM/cdControler.php';
 include_once __DIR__ . '/../../src/app/modelORM/empleadoControler.php';
 include_once __DIR__ . '/../../src/app/modelORM/estadoControler.php';
 include_once __DIR__ . '/../../src/app/modelORM/tipoControler.php';
 
 return function (App $app) {
     $container = $app->getContainer();
-
-    // A modo de ejemplo
-    $app->group('/cdORM', function () { 
-      $this->get('/', cdControler::class . ':TraerTodos'); //ok
-      $this->get('/cd/[{id}]', cdControler::class . ':TraerUno'); //ok
-      $this->post('/cd/add', cdControler::class . ':CargarUno'); //0k
-      $this->delete('/cd/delete/[{id}]', cdControler::class . ':BorrarUno');  
-      $this->put('/cd/[{id}]', cdControler::class . ':ModificarUno');   
-    });
 
     // Empleado
     $app->group('/empleado-orm', function () { 
