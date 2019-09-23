4bb58f5760c7165f43db6116053035ca0fe0c19f
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2019-09-22 22:11:46 -0300

    se agrega controlador de menu

---
+++ src/app/modelORM/menuControler.php
@@ -0,0 +1,61 @@
+<?php
+namespace App\Models\ORM;
+use App\Models\ORM\Menu;
+use App\Models\IApiControler;
+
+include_once __DIR__ . '/menu.php';
+include_once __DIR__ . '../../modelAPI/IApiControler.php';
+
+class MenuControler implements IApiControler {
+
+    public function TraerTodos($request, $response, $args) {
+        $todosLosTipos = Menu::all();
+        $newResponse = $response->withJson($todosLosTipos, 200);
+        return $newResponse;
+    }
+
+    public function TraerUno($request, $response, $args) {
+        $traerUno = Menu::find($args['id']);
+        $newResponse = $response->withJson($traerUno, 200);
+        return $newResponse;
+    }
+
+    public function CargarUno($request, $response, $args) {
+        $dato = json_decode(json_encode($request->getParsedBody()));
+
+        $miMenu = new Menu;
+        $miMenu->nombre_menu = $dato->dnombre_menu;
+        $miMenu->precio = $dato->precio;
+        $miMenu->id_tipos_sector = $dato->id_tipos_sector ;
+        $miMenu->descripcion_menu = $dato->descripcion_menu;
+        $miMenu->foto_menu = $dato->foto_menu;
+
+        $miMenu->save();
+
+        $newResponse = $response->withJson($miMenu, 200);
+        return $newResponse;
+    }
+
+    public function BorrarUno($request, $response, $args) {
+        $menu = Menu::destroy($args['id']);
+        $newResponse = $response->withJson($menu, 200);
+        return $newResponse;
+    }
+
+    public function ModificarUno($request, $response, $args) {
+        $dato = json_decode(json_encode($request->getParsedBody()));
+
+        $miMenu = Menu::find($args['id']);
+        $$miMenu->nombre_menu = $dato->dnombre_menu;
+        $miMenu->precio = $dato->precio;
+        $miMenu->id_tipos_sector = $dato->id_tipos_sector ;
+        $miMenu->descripcion_menu = $dato->descripcion_menu;
+        $miMenu->foto_menu = $dato->foto_menu;
+
+
+        $miMenu->save();
+
+        $newResponse = $response->withJson($miMenu, 200);
+        return $newResponse;
+    }
+}
