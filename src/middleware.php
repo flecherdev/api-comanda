24c97b982fe3426e5cf2f31f450341998b22afdb
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-01-07 18:57:16 -0300

    se modifica cors para conectar con proyecto angular http://localhost:4200

--- src/middleware.php
+++ src/middleware.php
@@ -93,7 +93,7 @@ return function (App $app) {
 	$app->add(function ($req, $res, $next) {
 		$response = $next($req, $res);
 		return $response
-			->withHeader('Access-Control-Allow-Origin', $this->get('settings')['cors'])
+			->withHeader('Access-Control-Allow-Origin', 'http://localhost:4200')
 			->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
 			->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 	});
