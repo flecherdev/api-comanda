8b920774420c03441c3ec9dd1ad9e781b50baeb1
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-02-25 00:02:30 -0300

    mofico metodo traerFecha y modificarUno

--- src/app/controlers/pedidoControler.php
+++ src/app/controlers/pedidoControler.php
@@ -18,26 +18,61 @@ class PedidoControler implements IApiControler {
         // traer los pedidos por fecha
         $dato = json_decode(json_encode($request->getParsedBody()));
         $fechaPedido = new \DateTime($dato->fecha_serch);
-        $fecha = $fechaPedido->format('Y-m-d');
+        //$fecha = $fechaPedido->format('Y-m-d');
         $id_empleado = $dato->id_empleado;
-        $id_tipo = $dato->id_tipo;
+        $id_tipo = $dato->id_tipo; // que tipo de empleado soy
          
-        $datosFecha = Pedido::select(
-            'id_pedidos',
-            'codigo_pedido',
-            'id_estado',
-            'fecha_pedido',
-            'hora_inicio_pedido',
-            'hora_estimada_entrega_pedido',
-            'hora_entrega_pedido',
-            'id_mesa',
-            'id_menu',
-            'id_mozo',
-            'id_empleado',
-            'nombre_cliente'
-        )->where(
-            'fecha_pedido', 'like', $fecha
-        )->get();
+        if($id_tipo != 5) {
+            $datosFecha = Pedido::select( // en eloquent el where funciona como un and
+                'pedidos.id_pedidos',
+                'pedidos.codigo_pedido',
+                'pedidos.id_estado',
+                'estados.descripcion_estado',
+                'pedidos.fecha_pedido',
+                'pedidos.hora_inicio_pedido',
+                'pedidos.hora_estimada_entrega_pedido',
+                'pedidos.hora_entrega_pedido',
+                'pedidos.id_mesa',
+                'pedidos.id_menu',
+                'menus.nombre_menu',
+                'pedidos.id_mozo',
+                'pedidos.id_empleado',
+                'pedidos.nombre_cliente',
+                'pedidos.id_tipo_menu'
+            )->whereDate(
+                'pedidos.fecha_pedido', $fechaPedido //por fecha
+            )->where(
+                'pedidos.id_tipo_menu', $id_tipo // por tipo de empleado que refiere a que empleado se asigna
+            )->join(
+                'menus','menus.id_menu','=','pedidos.id_menu'
+            )->join(
+                'estados','estados.id_estado', '=', 'pedidos.id_estado'
+            )->orderBy('pedidos.id_pedidos', 'DESC')->get();
+        } else {
+            $datosFecha = Pedido::select( // en eloquent el where funciona como un and
+                'pedidos.id_pedidos',
+                'pedidos.codigo_pedido',
+                'pedidos.id_estado',
+                'estados.descripcion_estado',
+                'pedidos.fecha_pedido',
+                'pedidos.hora_inicio_pedido',
+                'pedidos.hora_estimada_entrega_pedido',
+                'pedidos.hora_entrega_pedido',
+                'pedidos.id_mesa',
+                'pedidos.id_menu',
+                'menus.nombre_menu',
+                'pedidos.id_mozo',
+                'pedidos.id_empleado',
+                'pedidos.nombre_cliente',
+                'pedidos.id_tipo_menu'
+            )->whereDate(
+                'pedidos.fecha_pedido', $fechaPedido //por fecha
+            )->join(
+                'menus','menus.id_menu','=','pedidos.id_menu'
+            )->join(
+                'estados','estados.id_estado', '=', 'pedidos.id_estado'
+            )->orderBy('pedidos.id_pedidos', 'DESC')->get();
+        }
 
         
         $newResponse = $response->withJson($datosFecha, 200);
@@ -60,7 +95,7 @@ class PedidoControler implements IApiControler {
         $miPedido = new Pedido;
         $miPedido->codigo_pedido= $dato->codigo_pedido;
         $miPedido->id_estado= $dato->id_estado;
-        $miPedido->fecha_pedido = $fechaPedido->format('Y-m-d H:i:s');
+        $miPedido->fecha_pedido = $fechaPedido->format('Y-m-d');
         $miPedido->hora_inicio_pedido = $horaInicio->format('Y-m-d H:i:s');
         $miPedido->hora_estimada_entrega_pedido= $horaEstimada->format('Y-m-d H:i:s');
         // $miPedido->hora_entrega_pedido= date($dato->hora_entrega_pedido);
@@ -69,6 +104,7 @@ class PedidoControler implements IApiControler {
         $miPedido->id_mozo= $dato->id_mozo;
         $miPedido->id_empleado= $dato->id_empleado;
         $miPedido->nombre_cliente= $dato->nombre_cliente;
+        $miPedido->id_tipo_menu= $dato->id_tipo_menu;
         
         $miPedido->save();
 
@@ -91,16 +127,19 @@ class PedidoControler implements IApiControler {
         $miPedido->fecha_pedido= $dato->fecha_pedido;
         $miPedido->hora_inicio_pedido= $dato->hora_inicio_pedido;
         $miPedido->hora_estimada_entrega_pedido= $dato->hora_estimada_entrega_pedido;
-        $miPedido->hora_entrega_pedido= $dato->hora_entrega_pedido;
+        //$miPedido->hora_entrega_pedido= $dato->hora_entrega_pedido;
         $miPedido->id_mesa= $dato->id_mesa;
         $miPedido->id_menu= $dato->id_menu;
         $miPedido->id_mozo= $dato->id_mozo;
         $miPedido->id_empleado= $dato->id_empleado;
-        $miPedido->id_nombre_cliente= $dato->id_nombre_cliente;
+        $miPedido->nombre_cliente= $dato->nombre_cliente;
+        $miPedido->id_tipo_menu= $dato->id_tipo_menu;
 
         $miPedido->save();
 
         $newResponse = $response->withJson($miPedido, 200);
         return $newResponse;
     }
+
+    
 }
