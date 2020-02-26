e24c075518c197365c5f2545545f5c0270ef5d11
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-02-25 21:06:53 -0300

    se agrega instancia dotenv

--- public/index.php
+++ public/index.php
@@ -13,6 +13,10 @@ require __DIR__ . '/../vendor/autoload.php';
 
 session_start();
 
+// Creo instancia para leer archivos .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
 // Instantiate the app
 $settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
